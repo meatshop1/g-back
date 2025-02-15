d225c7dbc95a6c06ca387a886ad32cfff67981d3
BRHM1 <brhm@std.mans.edu.eg>
2025-02-15 02:34:43 +0200

    Production Ready

---
+++ meatshop/settings/dev.py
@@ -0,0 +1,30 @@
+from .common import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = True
+SECRET_KEY = 'django-insecure-&r%wiycqoat+yr+$@z4zimi(e!2oxn!(*p8z=zbjagwv-6t12q'
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'meatshop',
+#         'HOST': 'localhost',
+#         'USER': os.environ['LOCAL_DB_USER'],
+#         'PASSWORD': os.environ['LOCAL_DB_PASSWORD'],
+#     }
+# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
+
---
+++ meatshop/settings/prod.py
@@ -0,0 +1,22 @@
+import os
+from .common import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = False
+# after deploying to production, you should change the * in allowed_hosts to your domain name
+ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
