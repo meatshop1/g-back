b38f58599cbfe903fa009a95b750557760009054
BRHM1 <brhm@std.mans.edu.eg>
2025-02-17 08:06:27 +0200

    CORS

--- meatshop/settings/common.py
+++ meatshop/settings/common.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'meatshop.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://main.d11tq7hracfr3j.amplifyapp.com/"
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
