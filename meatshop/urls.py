bb5b7e0bbaf2ecabf9b63e88cdcbae172b2b5daa
abdelrhman eladwy <abdoahmed32522@gmail.com>
2025-05-13 12:39:20 +0300

    Update urls.py

--- meatshop/urls.py
+++ meatshop/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path , include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
